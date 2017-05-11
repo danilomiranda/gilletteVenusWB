dc9d224b3401ffbbbcb337fa4991191ec33a8e87
Danilo Miranda <danilokassio@gmail.com>
2017-05-11 19:07:42 -0300

    Put the images in place and animate one element

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/*external js
+http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js
+http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js
+*/
+window.onload = function(){
+ TweenLite.to("#ice1", 2, {left:200});
+}
